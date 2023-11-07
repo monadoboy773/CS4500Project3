9abb50b05fea8f7cb14195f054f9e50e9d37b212
Jaylen McKinney <69706581+monadoboy773@users.noreply.github.com>
2023-11-06 20:17:17 -0700

    Add files via upload

---
+++ Project3/Part2/producer_consumer.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 12
+#define FILE_NAME "message.txt"
+
+// circular queue
+char buffer[BUFFER_SIZE];
+int fill = 0;
+int use = 0;
+int count = 0;
+
+// condition variables to sync threads and a mutex
+pthread_cond_t empty, fill_c;
+pthread_mutex_t mutex;
+
+// function declarations
+void put(char val);
+char get();
+
+// Producer thread
+void* producer(void* arg) {
+    FILE *file = fopen(FILE_NAME, "r");
+    char c;
+
+    // open file
+    if(file == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    // read from file and put into buffer
+    while ((c = fgetc(file)) != EOF) {
+        pthread_mutex_lock(&mutex);
+        //wait if buffer is full
+        while(count == BUFFER_SIZE){
+            printf("Buffer is full. producer waits.\n");
+            pthread_cond_wait(&empty, &mutex);
+        }
+        // put the character into buffer
+        put(c);
+        // signal that buffer is not empty
+        pthread_cond_signal(&fill_c);
+        pthread_mutex_unlock(&mutex);
+        usleep(100000);
+    }
+
+    // close file
+    fclose(file);
+    return NULL;
+}
+
+// Consumer thread
+void* consumer(void* arg) {
+    while(1) {
+        pthread_mutex_lock(&mutex);
+        // wait if buffer is empty
+        while(count == 0){
+            printf("Buffer is empty. Consumer waits.\n");
+            pthread_cond_wait(&fill_c, &mutex);
+        }
+        // get a character from buffer
+        char value = get();
+        // signal that buffer is not full
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
+        // print character
+        printf("%c", value);
+        usleep(100000);
+        fflush(stdout);
+    }
+    return NULL;
+}
+
+// Function for putting a char into the buffer
+void put(char val) {
+    buffer[fill] = val;
+    fill = (fill + 1) % BUFFER_SIZE;
+    count++;
+}
+
+// Function to get char from buffer
+char get() {
+    char tmp = buffer[use];
+    use = (use + 1) % BUFFER_SIZE;
+    count--;
+    return tmp;
+}
+
+int main() {
+
+    // pthreads for producer and consumer
+    pthread_t producer_thread, consumer_thread;
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&fill_c, NULL);
+    pthread_mutex_init(&mutex, NULL);
+
+    // create producer and consumer threads
+    pthread_create(&producer_thread, NULL, producer, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
+
+    // let threads finish
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+
+    // destroy condition variables
+    pthread_cond_destroy(&empty);
+    pthread_cond_destroy(&fill_c);
+    pthread_cond_destroy(&mutex);
+
+    return 0;
+}
---
+++ Project3/string/substring_parallel.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+#define MAX 1024
+#define NUM_THREADS 4
+int total = 0;
+int n1,n2;
+char *s1,*s2;
+FILE *fp;
+
+pthread_mutex_t lock;
+
+int readf(FILE *fp)
+{
+	if((fp=fopen("strings.txt", "r"))==NULL){
+		printf("ERROR: can't open string.txt!\n");
+		return 0;
+	}
+	s1=(char *)malloc(sizeof(char)*MAX);
+	if(s1==NULL){
+		printf("ERROR: Out of memory!\n");
+		return -1;
+	}
+	s2=(char *)malloc(sizeof(char)*MAX);
+	if(s2==NULL){
+		printf("ERROR: Out of memory\n");
+		return -1;
+	}
+	/*read s1 s2 from the file*/
+	s1=fgets(s1, MAX, fp);
+	s2=fgets(s2, MAX, fp);
+	n1=strlen(s1)-1;  /*length of s1*/
+	n2=strlen(s2)-1; /*length of s2*/
+	
+	if(s1==NULL || s2==NULL || n1<n2)  /*when error exit*/
+		return -1;
+}
+
+void *num_substring(void *num)
+{
+	int i,j,k;
+	int count;
+	int start = *((int *)num);
+	int end = start + (n1/4) - n2 + 1;
+
+	for(i = start; i <= end; i++){   
+		count = 0;
+		for(j = i,k = 0; k < n2; j++,k++){  /*search for the next string of size of n2*/  
+			if (*(s1+j)!=*(s2+k)){
+				break;
+			}
+			else
+				count++;
+			if(count==n2){
+				pthread_mutex_lock(&lock);
+				total++;		/*find a substring in this step*/  
+				printf("total substrings updated: %d\n", total);                        
+				pthread_mutex_unlock(&lock);
+			}
+		}
+	}
+	pthread_exit(NULL);
+	//return (void *) total;
+}
+
+int main(int argc, char *argv[])
+{
+	int count;
+	readf(fp);
+	
+	pthread_t threads[NUM_THREADS]; // Creates an array of threads
+	int thread_num[NUM_THREADS];
+
+	pthread_mutex_init(&lock, NULL);
+
+	int i;
+	for(i = 0; i< NUM_THREADS; i++)
+	{
+		// Initializes a new thread
+		// &threads[i]: Assigns id to each index in array
+		// NULL: uses default thread attributes
+		// &num_substring: the function that the thread executes
+		// (void *) &thread_num[i]: The input to the function
+		thread_num[i] = i * (n1/4);
+		pthread_create(&threads[i],NULL, num_substring, &thread_num[i]);
+		printf("Thread %d created\n", i);
+	}
+
+	for(i = 0; i < NUM_THREADS; i++)
+	{
+		// join threads
+		pthread_join(threads[i],NULL);
+	}
+
+	pthread_mutex_destroy(&lock);
+
+	
+ 	printf("The number of substrings is: %d\n", total);
+	return 1;
+}
